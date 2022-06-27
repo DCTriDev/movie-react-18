2eb97f1f738ed80115aff9235eed5c139acbcadf
DinhCongTri <dinhcongtri0905@gmail.com>
2022-06-27 12:40:00 +0700

    change amount PopUpSearch render per time

--- src/Components/Navbar/SearchBox/PopUpSearch.jsx
+++ src/Components/Navbar/SearchBox/PopUpSearch.jsx
@@ -1,9 +1,10 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {List} from "antd";
 import {useHistory} from "react-router-dom";
 
 function PopUpSearch(props) {
     const history = useHistory()
+    const [renderAmount, setRenderAmount] = useState(5)
     const {searchResults} = props;
     useEffect(() => {
         const error = document.querySelector('.ant-empty-description');
@@ -18,16 +19,47 @@ function PopUpSearch(props) {
                 bordered
                 className='absolute z-50 bg-amber-50 rounded-2xl min-w-full overflow-hidden'
                 dataSource={searchResults ? searchResults : []}
-                renderItem={(item) => (
-                    <List.Item
-                        className='duration-200 hover:bg-amber-200 ease-in-out cursor-pointer'
-                        onClick={() => {
-                            history.push(`/movie-details/${item.maPhim}`)
-                        }}
-                    >
-                        {item.tenPhim}
-                    </List.Item>
-                )}
+                renderItem={(item, index) => {
+                    if (index < renderAmount) {
+                        return (
+                            <List.Item
+                                className='duration-200 hover:bg-amber-200 ease-in-out cursor-pointer'
+                                onClick={() => {
+                                    history.push(`/movie-details/${item.maPhim}`)
+                                }}
+                            >
+                                {item.tenPhim}
+                            </List.Item>
+                        )
+                    } else {
+                        if(index< renderAmount ) {
+                            return (
+                                <>
+                                    <List.Item
+                                        className='duration-200 hover:bg-amber-200 ease-in-out cursor-pointer'
+                                        onClick={() => {
+                                            history.push(`/movie-details/${item.maPhim}`)
+                                        }}
+                                    >
+                                        {item.tenPhim}
+                                    </List.Item>
+                                </>
+                            )
+                        }
+                        else {
+                            if(index===renderAmount+1) return (
+                                <List.Item
+                                    className='duration-200 hover:bg-amber-200 ease-in-out cursor-pointer text-center'
+                                    onClick={() => {
+                                        setRenderAmount(renderAmount + 5)
+                                    }}
+                                >
+                                    Hiển thị thêm
+                                </List.Item>
+                            )
+                        }
+                    }
+                }}
             />
         </>
     );
