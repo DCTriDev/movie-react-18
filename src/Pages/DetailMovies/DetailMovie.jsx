98b3f92bf49ef16984e4161da29ffb321c954a7f
DinhCongTri <dinhcongtri0905@gmail.com>
2022-08-08 18:23:05 +0700

    initial DetailMovie and ItemActor component

--- src/Pages/DetailMovies/DetailMovie.jsx
+++ src/Pages/DetailMovies/DetailMovie.jsx
@@ -1,67 +1,81 @@
-import React, {useEffect, useState} from 'react';
-import moment from "moment";
-import ReactPlayer from "react-player";
-import {useParams} from "react-router-dom";
-import movieService from "../../API/movieAPI";
+import React, {useEffect, useState} from 'react'
+import ReactPlayer from 'react-player'
+import {useParams} from 'react-router-dom'
+import movieService from '../../API/movieAPI'
+import moment from 'moment'
+import {ButtonCustom} from '../../Components/ButtonCustom/ButtonCustom'
+import ItemActor from './ItemActor/ItemActor'
+
+const {ButtonSecondary} = ButtonCustom
+
+const actor = [
+    {
+        id: 1,
+        name: 'Denise Richards',
+        image: 'https://m.media-amazon.com/images/M/MV5BMTQyNjYxNDU5OV5BMl5BanBnXkFtZTcwNTY5NDQwOA@@._V1_UX214_CR0,0,214,317_AL_.jpg',
+    },
+    {
+        id: 2,
+        name: 'Dolph Lundgren',
+        image: "https://m.media-amazon.com/images/M/MV5BMTUyMzEyNzU4NV5BMl5BanBnXkFtZTgwNDg2MzM3MDE@._V1_UX214_CR0,0,214,317_AL_.jpg"
+    },
+]
 
 function DetailMovie() {
-    const {maPhim} = useParams()
+    const {id} = useParams()
     const [detailMovies, setDetailMovies] = useState(null)
-    // useEffect(() => {
-    //     movieService.getMovieDetail(maPhim)
-    //         .then((res) => {
-    //             setDetailMovies(res.data.content)
-    //         })
-    //         .catch((err) => {
-    //             console.log(err)
-    //         })
-    // }, []);
+    useEffect(() => {
+        !detailMovies && movieService.getDetailMovie(id)
+            .then(res => {
+                setDetailMovies(res.data.getDetailMovie)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, [])
 
-    return (
-        detailMovies ? <div className='w-full bg-gradient-to-b from-sky-900 to-indigo-800 py-20 px-12'>
-            <div className='w-full grid grid-cols-8 gap-3'>
-                <div className='col-span-3 self-center h-full'>
-                    <img className='w-full h-3/4 object-cover rounded-lg' src={detailMovies.hinhAnh} alt="..."/>
-                </div>
-                <div className='col-span-5 px-4'>
-                    <h2 className='text-center text-3xl uppercase text-cyan-500 font-bold py-4'>{detailMovies.tenPhim}</h2>
-                    <p className='text-lg text-gray-300'>{detailMovies.moTa}</p>
-                    <p className='text-right italic text-amber-300 mr-16'>Đánh giá: {detailMovies.danhGia}/10</p>
-                    <div className=''>
-                        {
-                            detailMovies.dangChieu ? (
-                                <div className='flex flex-col items-center'>
-                                    <p className='py-6 text-lg text-white italic'>Ngày công
-                                        chiếu: {moment(detailMovies.ngayKhoiChieu).format('DD-MM-YYYY')}</p>
-                                    <button
-                                        className='bg-green-600 hover:bg-green-700 rounded-lg px-3 py-2 text-lg text-gray-50 transition-all duration-300'>Mua
-                                        Vé
-                                    </button>
-                                </div>
-                            ) : (
-                                <div className='flex flex-col items-center'>
-                                    <p className='py-6 text-lg text-white italic'>Ngày công
-                                        chiếu: {moment(detailMovies.ngayKhoiChieu).format('DD-MM-YYYY')}</p>
-                                    <button
-                                        className='bg-green-600 hover:bg-green-700 rounded-lg px-3 py-2 text-lg text-gray-50 transition-all duration-300'>Đăng
-                                        Ký Nhận Tin
-                                    </button>
-                                </div>
-                            )
-                        }
-                    </div>
-                </div>
-            </div>
-            {
-                detailMovies.trailer ? (
-                    <div className='w-full flex flex-col items-center justify-center p-20 space-y-6'>
-                        <h3 className='text-white text-2xl'>Trailer</h3>
-                        <ReactPlayer url={detailMovies.trailer} width="850px" height='480px'/>
-                    </div>
-                ) : (<></>)
+    const handleRenderCategory = (category) => {
+        const categoryLength = category?.length
+        return category?.map((item, index) => {
+            if (index === categoryLength - 1) {
+                return <span key={index}>{item.categoryName}</span>
             }
-        </div> : <></>
-    );
+            return <span key={index}>{item.categoryName} · </span>
+        })
+    }
+
+    console.log(detailMovies)
+
+    return (<div className='w-full px-12'>
+        <div className='grid grid-cols-8 py-5 lg:max-w-[1200px] mx-auto'>
+            <div className='col-span-5 flex flex-col items-center justify-center h-[625px]'>
+                <ReactPlayer controls={true} pip={true} url={detailMovies?.trailer} width='100%' height='100%' />
+            </div>
+            <div className='col-span-3 bg-black pl-4'>
+                <h3 className='text-xl text-text-color-title font-bold py-4'>{detailMovies?.title}</h3>
+                {detailMovies?.isPurchased ? <ButtonSecondary>Watch Now</ButtonSecondary> :
+                    <button>Purchase Now</button>}
+            </div>
+        </div>
+        <div className='max-w-[850px] mx-auto text-text-color-description py-4'>
+            <h3 className='text-xl text-text-color-title font-bold'>{detailMovies?.title}</h3>
+            <p className='px-2 py-0.5 bg-amber-500 rounded-xl text-black font-bold leading-10 inline'>{detailMovies?.type}</p>
+            <div className='leading-3'>
+                <p>Director: {detailMovies?.director}</p>
+                <p>Release Date: {moment(new Date(+detailMovies?.releaseDate)).format('DD/MM/YYYY')}</p>
+                <p>Category: {handleRenderCategory(detailMovies?.category)}</p>
+                <p className='mt-2 leading-5'>{detailMovies?.description}</p>
+            </div>
+            <h3 className='text-xl text-text-color-title font-bold mt-3'>Actor</h3>
+            <div className='grid grid-cols-4 gap-4'>
+                {
+                    actor.map((item, index) => {
+                        return <ItemActor key={index} actor={item} />
+                    })
+                }
+            </div>
+        </div>
+    </div>)
 }
 
 export default DetailMovie;
---
+++ src/Pages/DetailMovies/ItemActor/ItemActor.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+
+function ItemActor(props) {
+    const {id, name, image} = props.actor
+    return (
+        <NavLink className='overflow-hidden cursor-pointer text-text-color-primary hover:text-text-color-secondary ease-in-out duration-500 transition-all drop-shadow-2xl'
+                 to={`/actor/${id}`}
+        >
+            <img className='w-full h-[265px] object-cover rounded-xl' src={image} alt="image"/>
+            <h3 className='text-inherit text-ellipsis whitespace-nowrap mt-3 truncate'>{name}</h3>
+        </NavLink>
+    )
+}
+
+export default ItemActor
