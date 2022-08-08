1337f502388f5e5dd91fea39c94f474a90af2b77
DinhCongTri <dinhcongtri0905@gmail.com>
2022-08-08 18:18:32 +0700

    update movieSlice

--- src/Redux/Slice/movieSlice.jsx
+++ src/Redux/Slice/movieSlice.jsx
@@ -3,12 +3,12 @@ import movieService from "../../API/movieAPI";
 
 const initialState = {
     listMovie: null,
-    listCinema: null,
 }
 
 export const fetchListMovie = createAsyncThunk('movie/fetchListMovie', async () => {
-    const response = await movieService.getListMovies()
-    return response.data.content;
+    const response = await movieService.getAllMovie()
+    console.log(response.data.movie)
+    return response.data.movie;
 });
 
 const movieSlice = createSlice({
@@ -17,9 +17,6 @@ const movieSlice = createSlice({
     reducers: {
         setListMovie: (state, action) => {
             state.listMovie = action.payload
-        },
-        setListCinema: (state, action) => {
-            state.listCinema = action.payload
         },
     },
     extraReducers:(builder) => {
@@ -31,20 +28,12 @@ const movieSlice = createSlice({
 
 })
 
-export const setListMovieAction = () => {
-    return (dispatch) => movieService.getListMovies()
-        .then(res => {
-            dispatch(movieSlice.actions.setListMovie(res.data.content))
-        })
-        .catch(err => console.log(err))
-}
-
-export const setListCinemaAction = () => {
-    return (dispatch) => movieService.getListCinema()
-        .then(res => {
-            dispatch(movieSlice.actions.setListCinema(res.data.content))
-        })
-        .catch(err => console.log(err))
-}
+// export const setListCinemaAction = () => {
+//     return (dispatch) => movieService.getListCinema()
+//         .then(res => {
+//             dispatch(movieSlice.actions.setListCinema(res.data.content))
+//         })
+//         .catch(err => console.log(err))
+// }
 
 export default movieSlice.reducer;
