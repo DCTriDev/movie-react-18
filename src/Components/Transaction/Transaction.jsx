a5080cfc3074eb7870f7890eb2243dbec82112cd
DinhCongTri <dinhcongtri0905@gmail.com>
2022-08-12 01:34:57 +0700

    add Purchase action
    - add Result after Purchase.

--- src/Components/Transaction/Transaction.jsx
+++ src/Components/Transaction/Transaction.jsx
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from 'react'
 import ModalCustom from '../ModalCustom/ModalCustom'
 import {ButtonCustom} from '../ButtonCustom/ButtonCustom'
+import ResultCustom from '../ResultCustom/ResultCustom'
 import userService from '../../API/userAPI'
 import {useHistory} from 'react-router-dom'
 
-const {ButtonPrimary, ButtonDanger} = ButtonCustom
+const {ButtonPrimary, ButtonDanger, ButtonSubmit} = ButtonCustom
 
 function Transaction(props) {
     const history = useHistory()
@@ -12,6 +13,7 @@ function Transaction(props) {
     console.log(detailMovie)
     const [confirmLoading, setConfirmLoading] = useState(false)
     const [balance, setBalance] = useState(0)
+    const [isResultVisible, setIsResultVisible] = useState(false)
 
     const handleOk = () => {
         setConfirmLoading(true)
@@ -26,21 +28,37 @@ function Transaction(props) {
         setVisible(false)
     }
 
+    const handlePurchase = () => {
+        userService.purchaseMovie(detailMovie)
+            .then(res => {
+                setIsResultVisible(true)
+                setBalance(res.data.purchaseMovie.accountBalance.balance)
+            })
+    }
+
     const handleRenderContent = () => {
-        if(detailMovie?.price > balance){
+        if (detailMovie?.price > balance) {
             return (
                 <div className='flex flex-col items-center justify-between'>
-                    <p className='text-center text-[16px] text-text-color-secondary'>You don't have enough money to buy this movie.</p>
+                    <p className='text-center text-[16px] text-text-color-secondary'>You don't have enough money to buy
+                        this movie.</p>
                     <ButtonPrimary onClick={() => {
                         history.push('/deposit')
-                    }} >Deposit</ButtonPrimary>
+                    }}>Deposit</ButtonPrimary>
                 </div>
             )
-        }else {
+        } else {
             return (
                 <>
-                    <p className='text-center text-xl text-text-color-secondary'>You will be charged {detailMovie?.price}$ for this movie.</p>
-                    <p className='text-center text-xl text-text-color-secondary'>Are you sure you want to buy this movie?</p>
+                    <p className='text-center text-xl text-text-color-secondary'>You will be
+                        charged {detailMovie?.price}$ for this movie.</p>
+                    <ButtonPrimary
+                        onClick={() => {
+                            handlePurchase()
+                        }}
+                    >
+                        Purchase
+                    </ButtonPrimary>
                 </>
             )
         }
@@ -56,32 +74,62 @@ function Transaction(props) {
     }, [])
 
     return (
-        <ModalCustom
-            title='Transaction'
-            visible={visible}
-            onOk={handleOk}
-            confirmLoading={confirmLoading}
-            onCancel={handleCancel}
-            footer={[<ButtonDanger onClick={handleCancel}>Cancel</ButtonDanger>]}
-        >
-            <h3 className='text-text-color-title'>{detailMovie?.title}</h3>
-            <div className='flex px-10 gap-5'>
-                <img src={detailMovie?.image} alt='movie image' className='h-52' />
-                <div className='grid grid-cols-3 w-full'>
-                    <div className='col-span-2'>
-                        <p>Account Balance</p>
-                        <p>Price</p>
-                    </div>
-                    <div className='col-span-1 text-right'>
-                        <p>{balance}$</p>
-                        <p>{detailMovie?.price}$</p>
-                    </div>
-                    <div className='col-span-3'>
-                        {handleRenderContent()}
+        <>
+            <ModalCustom
+                title='Transaction'
+                visible={visible}
+                onOk={handleOk}
+                confirmLoading={confirmLoading}
+                onCancel={handleCancel}
+                footer={[<ButtonDanger onClick={handleCancel}>Cancel</ButtonDanger>]}
+            >
+                <h3 className='text-text-color-title'>{detailMovie?.title}</h3>
+                <div className='flex px-10 gap-5'>
+                    <img src={detailMovie?.image} alt='movie image' className='h-52' />
+                    <div className='grid grid-cols-3 w-full'>
+                        <div className='col-span-2'>
+                            <p>Account Balance</p>
+                            <p>Price</p>
+                        </div>
+                        <div className='col-span-1 text-right'>
+                            <p>{balance}$</p>
+                            <p>{detailMovie?.price}$</p>
+                        </div>
+                        <div className='col-span-3 flex flex-col justify-between w-full'>
+                            {handleRenderContent()}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </ModalCustom>
+            </ModalCustom>
+            <ModalCustom
+                title={null}
+                footer={null}
+                visible={isResultVisible}
+            >
+                <ResultCustom
+                    status='success'
+                    title='Successfully!'
+                    subTitle={[`Your reaming balance is ${balance}$`]}
+                    extra={[
+                        <ButtonPrimary type="primary" key="console"
+                                       onClick={() => {
+                                           history.push('/')
+                                       }}
+                        >
+                            Back to Homepage
+                        </ButtonPrimary>,
+                        <ButtonSubmit
+                            type='primary' key='console'
+                            onClick={() => {
+                                history.push(`/watch/${detailMovie?.id}`)
+                            }}
+                        >
+                            Watch Now
+                        </ButtonSubmit>,
+                    ]}
+                />
+            </ModalCustom>
+        </>
     )
 }
 
