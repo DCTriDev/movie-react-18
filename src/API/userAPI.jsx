d90551d05083111ede8ef204de47dd56a3a68c69
DinhCongTri <dinhcongtri0905@gmail.com>
2022-08-13 14:13:39 +0700

    add API
    - Add getGender API
    - Add signup API

--- src/API/userAPI.jsx
+++ src/API/userAPI.jsx
@@ -1,6 +1,6 @@
-import Axios from "./axiosClient";
-import { print } from 'graphql';
-import gql from 'graphql-tag';
+import Axios from './axiosClient'
+import {print} from 'graphql'
+import gql from 'graphql-tag'
 
 const LOG_IN = gql`
     mutation login($username:String!, $password: String) {
@@ -12,6 +12,32 @@ const LOG_IN = gql`
             phoneNumber
             roleName
             accessToken
+        }
+    }
+`
+
+const SIGN_UP = gql`
+    mutation signup(
+        $username:String!,
+        $password: String,
+        $fullName: String,
+        $email: String,
+        $phoneNumber: String,
+        $birthday: String,
+        $genderId: Int,
+    ) {
+        signup(input:{
+            username: $username,
+            password: $password,
+            fullName: $fullName,
+            email: $email,
+            phoneNumber: $phoneNumber,
+            birthday: $birthday,
+            genderId: $genderId,
+        }) {
+            username
+            password
+            roleName
         }
     }
 `
@@ -43,44 +69,76 @@ const PURCHASE_MOVIE = gql`
     }
 `
 
+const GET_GENDER = gql`
+    query gender {
+        gender{
+            id
+            gender
+        }
+    }
+`
+
 class UserAPI {
     login = (data, setLoading = true) => {
         const payload = {
             query: print(LOG_IN),
-            variables:{
+            variables: {
                 username: data.username,
-                password: data.password
-            }
+                password: data.password,
+            },
         }
-        return Axios.postMethod(payload, setLoading);
-    };
+        return Axios.postMethod(payload, setLoading)
+    }
 
     getAccountBalance = (setLoading = true) => {
         const payload = {
-            query: print(GET_ACCOUNT_BALANCE)
+            query: print(GET_ACCOUNT_BALANCE),
         }
-        return Axios.postMethod(payload, setLoading);
+        return Axios.postMethod(payload, setLoading)
     }
 
     deposit = (data, setLoading = true) => {
         const payload = {
             query: print(DEPOSIT),
-            variables:{
-                amount: data.amount
-            }
+            variables: {
+                amount: data.amount,
+            },
         }
-        return Axios.postMethod(payload, setLoading);
+        return Axios.postMethod(payload, setLoading)
     }
 
     purchaseMovie = (data, setLoading = true) => {
         const payload = {
             query: print(PURCHASE_MOVIE),
-            variables:{
+            variables: {
                 movieId: data.id,
-                promoCode: data.promoCode
-            }
+                promoCode: data.promoCode,
+            },
         }
-        return Axios.postMethod(payload, setLoading);
+        return Axios.postMethod(payload, setLoading)
+    }
+
+    getGender = (setLoading = true) => {
+        const payload = {
+            query: print(GET_GENDER),
+        }
+        return Axios.postMethod(payload, setLoading)
+    }
+
+    signup = (data, setLoading = true) => {
+        const payload = {
+            query: print(SIGN_UP),
+            variables: {
+                username: data.username,
+                password: data.password,
+                fullName: data.fullName,
+                email: data.email,
+                phoneNumber: data.phoneNumber,
+                birthday: data.birthday,
+                genderId: data.genderId,
+            },
+        }
+        return Axios.postMethod(payload, setLoading)
     }
 }
 
