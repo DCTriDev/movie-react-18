42c7c08583baa21e3bbbf6a8f09a20ca89060b96
DinhCongTri <dinhcongtri0905@gmail.com>
2022-09-15 18:29:56 +0700

    add content

--- src/Pages/WatchMovie/WatchMovie.jsx
+++ src/Pages/WatchMovie/WatchMovie.jsx
@@ -4,27 +4,34 @@ import {useHistory, useParams} from 'react-router-dom'
 import movieService from '../../API/movieAPI'
 import {ButtonCustom} from '../../Components/ButtonCustom/ButtonCustom'
 import moment from 'moment/moment'
+import ItemActor from '../DetailMovies/ItemActor/ItemActor'
 
-const {ButtonSquare, ButtonPrimary} = ButtonCustom
+const {ButtonSquare} = ButtonCustom
 
 function WatchMovie(props) {
     const {id} = useParams()
     const history = useHistory()
     const [detailMovies, setDetailMovies] = useState(null)
-    const [source, setSource] = useState(null)
+    const [source, setSource] = useState([])
     const [url, setUrl] = useState(null)
 
+    console.log(detailMovies)
+
     const handleRenderListSourceBtn = (source) => {
-        return source?.map((item, index) => {
-            return (
-                <ButtonSquare key={index} onClick={() => {
+        if(source.length > 0) {
+            return source?.map((item, index) => {
+                return (
+                    <ButtonSquare key={index} onClick={() => {
 
-                }}>
-                    {item.detailSource}
-                </ButtonSquare>
-            )
+                    }}>
+                        {item.detailSource}
+                    </ButtonSquare>
+                )
 
-        })
+            })
+        }else {
+            return <>No source found!</>
+        }
     }
 
     const handleErrorWithSource = (res) => {
@@ -33,6 +40,22 @@ function WatchMovie(props) {
                 history.push(`/detail-movie/${id}`)
             },3000)
         }
+    }
+
+    const handleRenderCategory = (category) => {
+        const categoryLength = category?.length
+        return category?.map((item, index) => {
+            if (index === categoryLength - 1) {
+                return <span key={index}>{item.categoryName}</span>
+            }
+            return <span key={index}>{item.categoryName} · </span>
+        })
+    }
+
+    const handleRenderActor = (actor) => {
+        return actor?.map((item, index) => {
+            return <ItemActor key={index} actor={item} />
+        })
     }
 
     useEffect(() => {
@@ -47,14 +70,20 @@ function WatchMovie(props) {
                 console.log(err)
             })
     })
-    console.log(source)
-    console.log(url)
-
     return (
         <div className='w-full px-12'>
             <div className='grid grid-cols-8 py-5 lg:max-w-[1280px] mx-auto h-[600px]'>
                 <div className='col-span-5 flex flex-col items-center justify-center h-full '>
-                    <ReactPlayer controls={true} pip={true} url={url} width='100%' height='100%' />
+                    {
+                        source.length >0
+                            ?
+                            <ReactPlayer controls={true} pip={true} url={url} width='100%' height='100%' />
+                            :
+                            <div className='bg-black w-full h-full flex flex-col justify-center items-center'>
+                                <p>No data found!</p>
+                                <p>Please try again later!</p>
+                            </div>
+                    }
                 </div>
                 <div className='col-span-3 bg-black p-4 h-full'>
                     <div className='flex flex-col h-full items-center'>
@@ -73,15 +102,15 @@ function WatchMovie(props) {
                 <div className='leading-3'>
                     <p>Director: {detailMovies?.director}</p>
                     <p>Release Date: {moment(new Date(+detailMovies?.releaseDate)).format('DD/MM/YYYY')}</p>
-                    {/*<p>Category: {handleRenderCategory(detailMovies?.category)}</p>*/}
+                    <p>Category: {handleRenderCategory(detailMovies?.category)}</p>
                     <p className='mt-2 leading-5'>{detailMovies?.description}</p>
                 </div>
-                {/*<h3 className='text-xl text-text-color-title font-bold mt-3'>Actor</h3>*/}
-                {/*<div className='grid grid-cols-4 gap-4'>*/}
-                {/*    {*/}
-                {/*        // handleRenderActor(detailMovies?.actor)*/}
-                {/*    }*/}
-                {/*</div>*/}
+                <h3 className='text-xl text-text-color-title font-bold mt-3'>Actor</h3>
+                <div className='grid grid-cols-4 gap-4'>
+                    {
+                        handleRenderActor(detailMovies?.actor)
+                    }
+                </div>
             </div>
         </div>
     )
