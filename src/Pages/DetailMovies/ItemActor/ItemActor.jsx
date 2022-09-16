3d1e0956cf2a62095f34e6eb4a30bca186712032
DinhCongTri <dinhcongtri0905@gmail.com>
2022-09-16 17:27:57 +0700

    responsive for sm and md screen

--- src/Pages/DetailMovies/ItemActor/ItemActor.jsx
+++ src/Pages/DetailMovies/ItemActor/ItemActor.jsx
@@ -7,7 +7,7 @@ function ItemActor(props) {
         <NavLink className='overflow-hidden cursor-pointer text-text-color-primary hover:text-text-color-secondary ease-in-out duration-500 transition-all drop-shadow-2xl'
                  to={`/actor/${id}`}
         >
-            <img className='w-full object-contain rounded-xl object-left' src={image} alt="image"/>
+            <img className='w-full lg:h-[265px] md:h-[180px] h-[120px] object-contain rounded-xl object-left' src={image} alt="image"/>
             <h3 className='text-inherit lg:text-[16px] md:text-[15px] text-[14px] text-ellipsis whitespace-nowrap mt-3 truncate'>{name}</h3>
         </NavLink>
     )
