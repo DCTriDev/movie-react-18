0f2b2e3caab47b491558a8ed6d5e88ae05574a91
DinhCongTri <dinhcongtri0905@gmail.com>
2022-09-16 15:53:49 +0700

    change css of antd dropdown menu

--- src/Components/UserMenu/UserMenu.jsx
+++ src/Components/UserMenu/UserMenu.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import {Menu} from "antd";
 
 const UserMenu = styled(Menu)`
-  .UGnHX .ant-dropdown-menu-item, .UGnHX .ant-dropdown-menu-submenu-title {
+  .ant-dropdown-menu-item, .ant-dropdown-menu-submenu-title {
     margin: -2px !important;
     padding: 4px !important;
   }
@@ -20,7 +20,6 @@ const UserMenu = styled(Menu)`
   }
 
   .ant-dropdown-menu-item.ant-dropdown-menu-item-active, .ant-dropdown-menu-item.ant-dropdown-menu-submenu-title-active, .ant-dropdown-menu-item:hover, .ant-dropdown-menu-submenu-title.ant-dropdown-menu-item-active, .ant-dropdown-menu-submenu-title.ant-dropdown-menu-submenu-title-active, .ant-dropdown-menu-submenu-title:hover {
-    
     border-radius: 10px;
     color: #f5f5f5;
     transition: background-color 0.1s ease-in-out !important;
