0da2e1d411a7ba67396a61e9c9adc0769d896324
DinhCongTri <dinhcongtri0905@gmail.com>
2022-09-16 15:42:44 +0700

    fetchListMovie if listMovie is null

--- src/Components/Navbar/SearchBox/SearchBox.jsx
+++ src/Components/Navbar/SearchBox/SearchBox.jsx
@@ -1,12 +1,14 @@
 import React, {useEffect, useState} from 'react';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from 'react-redux'
 import PopUpSearch from "./PopUpSearch";
 import SearchBoxOutline from "./SearchBoxOutline"
+import {fetchListMovie} from '../../../Redux/Slice/movieSlice'
 
 function SearchBox() {
     const [searchTerm, setSearchTerm] = useState('')
     const [searchResults, setSearchResults] = useState(null)
     const listMovie = useSelector(state => state.movieSlice.listMovie)
+    const dispatch = useDispatch()
     useEffect(() => {
         const delayDebounceFn = setTimeout(() => {
             if (searchTerm.length > 0) {
@@ -25,6 +27,11 @@ function SearchBox() {
     const handleSearch = (e) => {
         setSearchTerm(e.target.value)
     }
+
+    useEffect(() => {
+        !listMovie&&dispatch(fetchListMovie())
+    }, [listMovie])
+
 
     return (
         <div className='relative'>
