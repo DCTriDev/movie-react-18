8df88b1d942b0ac7efd79ad59040a9ae68c38470
DinhCongTri <dinhcongtri0905@gmail.com>
2022-09-16 15:48:10 +0700

    change css of antd dropdown menu

--- src/Components/Navbar/UserNav/UserNav.jsx
+++ src/Components/Navbar/UserNav/UserNav.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import localService from "../../../Services/local.service";
-import {useHistory} from "react-router-dom";
-import {removeUserInfo} from "../../../Redux/Slice/userSlice";
-import {Dropdown} from 'antd';
-import {ButtonCustom} from '../../ButtonCustom/ButtonCustom';
-import UserMenu from '../../UserMenu/UserMenu';
+import React from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import localService from '../../../Services/local.service'
+import {useHistory} from 'react-router-dom'
+import {removeUserInfo} from '../../../Redux/Slice/userSlice'
+import {Dropdown} from 'antd'
+import {ButtonCustom} from '../../ButtonCustom/ButtonCustom'
+import UserMenu from '../../UserMenu/UserMenu'
 
 const {ButtonPrimary} = ButtonCustom
 
@@ -25,6 +25,45 @@ function UserNav(props) {
         history.push('/login')
     }
 
+    const handleRenderUserNav = () => {
+        return isDesktop
+            ?
+            (
+                <div>
+                    <span className='pr-2.5'>Hi, <strong
+                        className='text-text-color-secondary'>{userInfo.username}</strong></span>
+                    <Dropdown
+                        className='mr-6'
+                        overlay={localService.isAdmin() ? menuADMIN : menu} placement='bottom'
+                        arrow={isDesktop}
+                        overlayClassName={
+                            isDesktop ? '' : 'left-[unset] right-[4px]'
+                        }
+                    >
+                        <img
+                            className='rounded-full w-10 h-10 cursor-pointer'
+                            src={userInfo.avatar} alt='avatar' />
+                    </Dropdown>
+                </div>
+            )
+            :
+            (
+                <Dropdown
+                    className=''
+                    overlay={localService.isAdmin() ? menuADMIN : menu} placement='bottom'
+                    arrow={isDesktop}
+                    overlayClassName={
+                        isDesktop ? '' : 'left-[unset] right-[4px]'
+                    }
+                >
+                    <img
+                        className='rounded-full w-10 h-10 cursor-pointer'
+                        src={userInfo.avatar} alt='avatar' />
+                </Dropdown>
+            )
+
+    }
+
     const menu = (
         <UserMenu
             className='rounded-xl p-[4px] m-[-4px] w-fit bg-black'
@@ -32,8 +71,9 @@ function UserNav(props) {
                 {
                     key: '1',
                     label: (
-                        <a className='flex items-center space-x-2 text-[16px]' rel='prefetch' href="/profile">
-                            <ion-icon className='' name="person-outline"/>
+                        <a className='flex items-center space-x-2 lg:text-[16px] md:text-[14px]' rel='prefetch'
+                           href='/profile'>
+                            <ion-icon className='' name='person-outline' />
                             <span>Profile</span>
                         </a>
                     ),
@@ -41,15 +81,15 @@ function UserNav(props) {
                 {
                     key: '2',
                     label: (
-                        <a className='flex items-center space-x-2 text-[16px]' onClick={handleLogOut}>
-                            <ion-icon className='' name="log-out-outline"/>
+                        <a className='flex items-center space-x-2 lg:text-[16px] md:text-[14px]' onClick={handleLogOut}>
+                            <ion-icon className='' name='log-out-outline' />
                             <span>Logout</span>
                         </a>
                     ),
                 },
             ]}
         />
-    );
+    )
 
     const menuADMIN = (
         <UserMenu
@@ -58,8 +98,9 @@ function UserNav(props) {
                 {
                     key: '1',
                     label: (
-                        <a className='flex items-center space-x-2 text-[16px]' rel='prefetch' href="/profile">
-                            <ion-icon className='' name="person-outline"/>
+                        <a className='flex items-center space-x-2 lg:text-[16px] md:text-[14px]' rel='prefetch'
+                           href='/profile'>
+                            <ion-icon className='' name='person-outline' />
                             <span>Profile</span>
                         </a>
                     ),
@@ -67,8 +108,8 @@ function UserNav(props) {
                 {
                     key: '2',
                     label: (
-                        <a className='flex items-center space-x-2 text-[16px]' href="/dashboard">
-                            <ion-icon name="grid-outline"></ion-icon>
+                        <a className='flex items-center space-x-2 lg:text-[16px] md:text-[14px]' href='/dashboard'>
+                            <ion-icon name='grid-outline'></ion-icon>
                             <span>Dashboard</span>
                         </a>
                     ),
@@ -76,32 +117,20 @@ function UserNav(props) {
                 {
                     key: '3',
                     label: (
-                        <a className='flex items-center space-x-2 text-[16px]' onClick={handleLogOut}>
-                            <ion-icon className='' name="log-out-outline"/>
+                        <a className='flex items-center space-x-2 lg:text-[16px] md:text-[14px]' onClick={handleLogOut}>
+                            <ion-icon className='' name='log-out-outline' />
                             <span>Logout</span>
                         </a>
                     ),
                 },
             ]}
         />
-    );
+    )
 
     return (
         userInfo
             ?
-            (
-                <div>
-                    <span className='pr-2.5'>Hi, <strong className='text-text-color-secondary'>{userInfo.username}</strong></span>
-                    <Dropdown
-                        className='mr-6'
-                        overlay={localService.isAdmin()?menuADMIN:menu} placement="bottom"
-                        arrow>
-                        <img
-                            className='rounded-full w-10 h-10 cursor-pointer'
-                            src={userInfo.avatar} alt="avatar"/>
-                    </Dropdown>
-                </div>
-            )
+            handleRenderUserNav()
             :
             (
                 <>
@@ -112,8 +141,8 @@ function UserNav(props) {
                     </ButtonPrimary>
                 </>
             )
-    );
+    )
 }
 
 // export default React.memo(UserNav);
-export default UserNav;
+export default UserNav
