6835d130a76dd725a97a479f6f62ffc7fe755bcd
DinhCongTri <dinhcongtri0905@gmail.com>
2022-09-16 16:39:09 +0700

    responsive for sm and md screen

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 
 function Footer(props) {
     return (
-        <div className='w-full py-3 px-8 text-text-color-description bg-black'>
+        <div className='w-full lg:py-3 md:py-2 py-1 lg:px-8 px-2 text-text-color-description bg-black'>
             <div className='copyright flex justify-between items-center '>
-                <div>Copyright © 2022 All Rights Reserved by <span className='text-text-color-secondary'>49BigBox</span></div>
-                <div className='text-3xl space-x-2'>
-                    <a href='https://www.facebook.com/no.bodys.009' className='text-[#444444] hover:text-[#4267B2]'>
-                        <ion-icon name="logo-facebook"></ion-icon>
+                <div className='lg:text-[14px] md:text-[13px] text-[12px]'>
+                    <span>Copyright © 2022 <br /> All Rights Reserved by </span>
+                    <span className='text-text-color-secondary'>49BigBox</span>
+                </div>
+                <div className='text-3xl space-x-2 flex justify-between items-center'>
+                    <a href='https://www.facebook.com/no.bodys.009'
+                       className='flex justify-between items-center lg:text-[#444444] text-[#4267B2] hover:text-[#4267B2]'
+                    >
+                        <ion-icon name='logo-facebook'></ion-icon>
                     </a>
-                    <a href='https://www.youtube.com/channel/UCcUWN04I6NddvHOrbQuLWPQ' className='text-[#444444] hover:text-[#FF0000]'>
-                        <ion-icon name="logo-youtube"></ion-icon>
+                    <a href='https://www.youtube.com/channel/UCcUWN04I6NddvHOrbQuLWPQ'
+                       className='flex justify-between items-center lg:text-[#444444] text-[#FF0000] hover:text-[#FF0000]'
+                    >
+                        <ion-icon name='logo-youtube'></ion-icon>
                     </a>
                 </div>
             </div>
